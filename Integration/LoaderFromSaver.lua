afbf317967a30fb07a108647206c64397fc1564c
Danell <jacob@emberlight.se>
2021-01-19 16:10:08 +0100

    Fixed script to work with Prism paths

--- Integration/LoaderFromSaver.lua
+++ Integration/LoaderFromSaver.lua
@@ -1,41 +1,35 @@
 -- Description: 
--- Create a Loader from selected Saver node. Works with multiple Savers.
--- Features:
--- If the Saver has image sequence with no number padding, scan existing files and suggest correct sequence numbering 
--- If the file is a container (mov, avi, mp4, mxf) — original name is used. 
--- Launch this scripts with Saver Manager UI tool (available in Reactor) 
--- connect the created Loader to Saver's downstream nodes 
--- convert regular saver to Saver Plus 
--- add versioning buttons to Saver Plus 
---
+--         Create a Loaver from selected Saver node. Works with multiple selected selectedSavers.
 -- License: MIT
--- Author: Alexey Bogomolov
+-- Author: Alex Bogomolov
 -- email: mail@abogomolov.com
--- Donate: paypal.me/aabogomolov/2usd
--- Version: 1.2, 2020/27/11
+-- Donate: paypal.me/aabogomolov
+-- Version: 1.1, 2020/9/1
 
 comp = fu:GetCurrentComp()
-
 selectedSavers = comp:GetToolList(true, 'Saver')
 
-local platform = (FuPLATFORM_WINDOWS and 'Windows') or (FuPLATFORM_MAC and 'Mac') or (FuPLATFORM_LINUX and 'Linux')
-
-local listDirCmd = "ls "
-local suffixCmd = ""
-
-if platform == "Windows" then
-    listDirCmd = "dir "
-    suffixCmd = " /b"
+function place_loader(name, tool)
+    local flow = comp.CurrentFrame.FlowView
+    x, y = flow:GetPos(tool)
+    -- local loader = comp:Loader({ Clip = name })
+    local loader = comp:AddTool("Loader")
+    loader.Clip = name
+    flow:SetPos(loader, x, y+1)
+    inputs = tool.Output:GetConnectedInputs()
+    for i, input in ipairs(inputs) do
+        input:ConnectTo(loader.Output)
+    end
+    if not bmd.fileexists(name) then
+        print("file is not found ", name)
+        parseFile = bmd.parseFilename(name)
+    end
 end
 
-if comp:GetAttrs("COMPS_FileName") == "" then
-    print('save the comp!')
-end
 
-
-
-function isMovieFormat(extension)
-	if extension ~= nil then
+function pathIsMovieFormat(path)
+    local extension = bmd.parseFilename(path).Extension:lower()
+    if extension ~= nil then
         if  ( extension == ".3gp" ) or
             ( extension == ".aac" ) or
             ( extension == ".aif" ) or
@@ -63,39 +57,10 @@ function isMovieFormat(extension)
             ( extension == ".vpv" ) or
             ( extension == ".wav" ) or
             ( extension == ".webm" ) then
-			return true
-		end
-	end
-	return false
-end
-
-function findFiles(parsedPath)
-    files = assert(io.popen(listDirCmd .. comp:MapPath(parsedPath.Path) .. suffixCmd))
-
-    for file in files:lines() do
-        base, snum, ext = string.match(file,"^(.+[._-])(%d+)(%..+)$")
-        if base == parsedPath.CleanName then
-            return snum or "0000"
+            return true
         end
     end
-    files:close()
-    return nil
-
-end
-
-function placeAndReconnect(name, tool)
-    local flow = comp.CurrentFrame.FlowView
-    x, y = flow:GetPos(tool)
-    local loader = comp:AddTool("Loader")
-    loader.Clip = name
-    flow:SetPos(loader, x+1, y)
-    inputs = tool.Output:GetConnectedInputs()
-    for i, input in ipairs(inputs) do
-        input:ConnectTo(loader.Output)
-    end
-    if not bmd.fileexists(comp:MapPath(name)) then
-        print("file is not found ", name)
-    end
+    return false
 end
 
 if (#selectedSavers) == 0 then
@@ -104,36 +69,28 @@ else
     comp:Lock()
     comp:StartUndo('loader from saver')
     for _, tool in ipairs(selectedSavers) do
-        selectedClipName = tool.Clip[1]
-        if selectedClipName == "" then
-            print("Saver " .. tool.Name .. ": filename is empty!")
+        selected_clipname = tool:GetAttrs()["TOOLST_Clip_Name"][1]
+        if selected_clipname == "" then
+            print(tool.Name .. " filename is empty!")
         else
-            parsedFile = bmd.parseFilename(selectedClipName)
-            ext = parsedFile.Extension:lower()
-            path = parsedFile.Path
-            cleanName = parsedFile.CleanName
-            sequenceNumber = parsedFile.SNum 
+            match_sequence_number = string.match(selected_clipname, '(%d+)%..$')
+            --ext = string.match(selected_clipname, '(%.%w+)$')
+            --match_sequence_number = string.match(selected_clipname, '(%d+)' .. ext .. '$')
 
-            -- if it is a sequence, find actual seq number
-            if not isMovieFormat(ext) then
-                if not sequenceNumber or not bmd.fileexists(comp:MapPath(selectedClipName)) then
-                    suggestedSnum = findFiles(parsedFile)
-                    if not suggestedSnum then
-                        newClipName = selectedClipName
-                    else
-                        print("starting sequence number for the new loader is set to: ".. suggestedSnum)
-                        newClipName = path .. cleanName .. suggestedSnum .. ext
-                    end
-                else
-                    newClipName = path .. cleanName .. sequenceNumber .. ext
-                end
+            -- if pattern 0000.ext$ is found or file is a movie container, set the name as is
+            if match_sequence_number or pathIsMovieFormat(comp:MapPath(selected_clipname)) then
+                place_loader(selected_clipname, tool)
             else
-                newClipName = selectedClipName
+                name, ext = string.match(selected_clipname,'([^/]-([^.]+))$')
+                local new_name = selected_clipname:gsub('.'..ext, '0000.'..ext)
+                place_loader(new_name, tool)
             end
-
-            placeAndReconnect(newClipName, tool)
         end
     end
     comp:EndUndo()
     comp:Unlock()
+    if comp:GetAttrs("COMPS_FileName") == "" and comp:IsLocked() then
+        print('save the comp!')
+    end
 end
+
